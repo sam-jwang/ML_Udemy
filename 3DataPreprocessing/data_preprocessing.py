70488b4b65c55ee2b128798a1fc118cf1c637b15
Sam Wang <samuel.joshua.wang@gmail.com>
2021-06-01 03:18:07 -0500

    Learned encoding and splitting dataset

--- 3DataPreprocessing/data_preprocessing.py
+++ 3DataPreprocessing/data_preprocessing.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 # Trying to predict if someone will purchase something given:
 #   Country, Age, Salary
@@ -12,9 +16,6 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-print(X)
-print(y)
-
 # Takes care of nan, fills with mean
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 # Specifying columns w missing values, but in general good to just include all bc you don't know where 
@@ -23,4 +24,15 @@ imputer.fit(X[:, 1:3])
 # .transform() returns updated columns
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-print(X)
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
